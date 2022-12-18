7e5daa0b27f971122f8fd0e4d05872482ad3ef0e
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2022-12-18 20:07:34 +0000

    Unreachable operation optimization
    Add optimizer for eliminating unreachable operations.

--- funcy/ir/optimizer.py
+++ funcy/ir/optimizer.py
@@ -1,11 +1,40 @@
 from collections.abc import Callable
 
-from .code import Code
+from .code import Code, Op, OpType
+
+def is_op_terminator(op: Op) -> bool:
+    """
+    Get whether an IR operation is a terminator. Terminator operations
+    are operations that guarantee that any subsequent operations will
+    not be executed.
+    """
+    return op.type in (OpType.HALT, OpType.JUMP_LABEL, OpType.RETURN)
+
+
+def optimizer_eliminate_unreachable_ops(code: Code) -> bool:
+    """
+    Eliminates unreachable IR operations that follow a terminator
+    operation. Returns whether any optimization was performed.
+    """
+    
+    was_optimized: bool = False
+    
+    for block in code.blocks:
+        for terminator_index in range(len(block.ops) - 1):
+            if is_op_terminator(block.ops[terminator_index]):
+                block.ops = block.ops[:terminator_index + 1]
+                was_optimized = True
+                break
+    
+    return was_optimized
+
 
 def optimize_code(code: Code) -> None:
     """ Optimize an IR code program. """
     
-    OPTIMIZERS: list[Callable[[Code], bool]] = []
+    OPTIMIZERS: list[Callable[[Code], bool]] = [
+        optimizer_eliminate_unreachable_ops,
+    ]
     
     should_optimize: bool = True
     iterations: int = 256
